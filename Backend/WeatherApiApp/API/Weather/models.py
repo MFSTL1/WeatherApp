626fb3449ddbba62435f272e5dd66f97b224077d
Bartłomiej <134093917+MFSTL1@users.noreply.github.com>
2025-04-21 22:56:29 +0200

    further changes before inclusion of the db and the ui

--- Backend/WeatherApiApp/API/Weather/models.py
+++ Backend/WeatherApiApp/API/Weather/models.py
@@ -1,8 +1,12 @@
 from cachetools import TTLCache
 from django.db import models
 import cachetools
+import aes
 
-class TempStorage():
-    def __init__(self):
-        cache = TTLCache(maxsize=1, TTLCache=3600)
+class Storage(models.Model):
+    name = models.CharField(max_length=100)
+    encrypted_api_key = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 # Create your models here.
